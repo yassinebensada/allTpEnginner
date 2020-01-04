36ac97ef9d149ac8cb27abddcfed19850ded912e
Yassine BEN SADA <45922447+yassinebensada@users.noreply.github.com>
2020-01-04 18:07:39 +0100

    TP2 Exercice3

--- main.c
+++ main.c
@@ -1,37 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* alloue_vecteur(int length,int value)
+void affiche_vecteur(int* tab,int length)
 {
     int i;
-    int* vect = malloc(length*sizeof(int));
     for(i=0;i<length;i++)
-   {
-       *(vect+i)=value;
-   }
-   return vect ;
+    {
+        printf("Table[%d] : %d\t",i,*(tab+i));
+    }
 }
-int** alloue_matrice(int ligne,int column,int value)
+
+int** alloueMatricePascal(int dimension)
 {
     int i,j;
-    int** matrice = malloc(ligne*sizeof(int*));
-    for(i=0;i<ligne;i++)
+    int** matrice = malloc(dimension*sizeof(int*));
+    for (i=0;i<dimension;i++)
     {
-        *(matrice+i) = malloc(column*sizeof(int));
-    }
-    for(i=0;i<ligne;i++)
-    {
-        for(j=0;j<column;j++)
-        {
-            matrice[i][j]=value;
-        }
+        matrice[i]= malloc(i*sizeof(int));
     }
     return matrice;
-
+}
+int** remplitMatricePascal(int dimension)
+{
+     int i,j;
+     int** matrice = alloueMatricePascal(dimension);
+     for (i=0;i<dimension;i++)
+     {
+            matrice[i][0]=1;
+            matrice[i][i]=1;
+     }
+     for (i=2;i<dimension;i++)
+     {
+        for(j=1;j<i;j++)
+        {
+            *(*(matrice+i)+j)= *(*(matrice+(i-1))+j-1)+*(*(matrice+(i-1))+j);
+        }
+    }
+     return matrice;
+}
+void aﬃcheMatricePascal(int** matrice,int dimension)
+{
+    int i;
+    for(i=0;i<dimension;i++)
+        aﬃche_vecteur(matrice[i],i+1);
+    printf("\n");
 }
 int main()
 {
-    int** matrice = alloue_matrice(4,4,2);
 
     return 0;
 }
